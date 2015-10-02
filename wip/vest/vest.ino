da64a306625e9756cc57d382b5418ab050375072
Dany Shaanan <danyshaanan@gmail.com>
2015-10-02 04:58:07 +0300

    Add script for sensor control of FastLED strip's speed

---
+++ wip/vest/vest.ino
@@ -0,0 +1,39 @@
+
+// Led strip animation using FastLED
+// https://github.com/FastLED/FastLED
+
+#include "FastLED.h"
+#include <stdlib.h>
+
+#define BYTE           255         // FastLED channels are all bytes
+#define PIN            6           // data pin for led strip
+#define SENSORPIN      7           // sensor analog input pin
+#define NUMPIXELS      4           // number of leds on led strip
+#define fps            30          // frames per second
+
+#define maxSpeed       7
+
+int msPerFrame = 1000 / fps;
+
+int t;
+CRGB leds[NUMPIXELS];
+
+/////////////////////////////////////////////////////
+
+void setup() {
+  t = 0;
+  FastLED.addLeds<NEOPIXEL, PIN>(leds, NUMPIXELS);
+}
+
+void loop() {
+  for (int i=0; i<NUMPIXELS; i++) {
+    leds[i] = CRGB::Black;
+  }
+  leds[int(t / 1000) % NUMPIXELS] = CRGB::Red;
+  FastLED.show();
+
+  t += msPerFrame * map(analogRead(SENSORPIN), 0, 1024, 1, maxSpeed);
+  delay(msPerFrame);
+}
+
+//////////////////////////////////////////////////////////
