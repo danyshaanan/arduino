daf6ad4132b31f0cc51289835458bf45ea74d786
Dany Shaanan <danyshaanan@gmail.com>
2015-10-02 14:40:50 +0300

    Refactor and simplify accelerometer strip example

--- examples/accelerometer/strip/strip.ino
+++ examples/accelerometer/strip/strip.ino
@@ -4,47 +4,23 @@
 #include <stdlib.h>
 #include "FastLED.h" // https://github.com/FastLED/FastLED
 
-#define BYTE           255 // FastLED channels are all bytes
-#define NUMPIXELS      4 // number of leds on led strip
 #define PIXELSPIN      3 // data pin for led strip
-#define VALUE          0.4
-#define X              1 //analog pin
-#define Y              2 //analog pin
-#define Z              3 //analog pin
-#define VERBOSE        1
+#define VALUE          100 // out of 255
+#define DIMENTIONS     3 // number of axis in 3d space and leds on led strip
 
-CRGB leds[NUMPIXELS];
-
-int x = 0;
-int y = 0;
-int z = 0;
-
-int minVal = 320;
-int maxVal = 400;
-
-int asHue(int pin) {
-  return constrain(map(analogRead(pin), minVal, maxVal, 0, 255), 0, 255);
-}
-
-int nintyTen(int p, int q) {
-  return p * 0.7 + q * 0.3;
-}
+CRGB leds[DIMENTIONS];
+int axisSensorPins[] = { 1, 2, 3 };
+int axisValue[] = { 0, 0, 0 };
 
 void setup() {
-  FastLED.addLeds<WS2811, PIXELSPIN>(leds, NUMPIXELS);
-  if (VERBOSE) Serial.begin(9600);
+  FastLED.addLeds<WS2811, PIXELSPIN>(leds, DIMENTIONS);
 }
 
 void loop() {
-  x = nintyTen(x, asHue(X));
-  y = nintyTen(y, asHue(Y));
-  z = nintyTen(z, asHue(Z));
-
-  leds[0] = CHSV(0, BYTE, x * VALUE);
-  leds[1] = CHSV(0, BYTE, y * VALUE);
-  leds[2] = CHSV(0, BYTE, z * VALUE);
-  leds[3] = CRGB::Black;
-
+  for (int d=0; d<DIMENTIONS; d++) {
+    axisValue[d] = (axisValue[d] + analogRead(axisSensorPins[d])) / 2;
+    leds[d] = CRGB(axisValue[d] > 420 ? VALUE : 0, axisValue[d] < 300 ? VALUE : 0, 0);
+  }
   FastLED.show();
   delay(30);
 }
