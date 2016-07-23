daa03581bbb11051a2e919750c6dbd6ce0476788
Dany Shaanan <danyshaanan@gmail.com>
2016-07-23 16:25:55 +0300

    Update game-of-life

--- arduino/pixels/game-of-life/game-of-life.ino
+++ arduino/pixels/game-of-life/game-of-life.ino
@@ -29,33 +29,45 @@ int countNeighbors(int i, int j) {
   return neighbors;
 }
 
+void setOffGrid() {
+  for (int i = 0; i < ROWS; i++) for (int j = 0; j < COLS; j++) grid[i][j] = false;
+}
+
+void setRandomGrid() {
+  for (int i = 0; i < ROWS; i++) for (int j = 0; j < COLS; j++) grid[i][j] = rand() % 2;
+}
+
+void setGliderGrid() {
+  setOffGrid();
+  grid[0][0] = grid[0][1] = grid[0][2] = grid[1][2] = grid[2][1] = true;
+}
+
 void setup() {
-  srand(analogRead(0));
+  srand(analogRead(A0) + analogRead(A1) * analogRead(A2));
   pinMode(LED_BUILTIN, OUTPUT);
   FastLED.addLeds<WS2812, PIN>(leds, ROWS * COLS);
-  for (int i = 0; i < ROWS; i++) {
-    for (int j = 0; j < COLS; j++) {
-      grid[i][j] = rand() % 2;
-    }
-  }
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, grid[0][0]);
-  for (int i = 0; i < ROWS; i++) {
-    for (int j = 0; j < COLS; j++) {
-      setLedBrightness(i, j, grid[i][j] ? 4 : 0);
-      temp[i][j] = grid[i][j];
+  rand() % 4 ? setRandomGrid() : setGliderGrid();
+  for (int iteration = 0; iteration < 1000; iteration++) {
+
+    digitalWrite(LED_BUILTIN, grid[0][0]);
+    for (int i = 0; i < ROWS; i++) {
+      for (int j = 0; j < COLS; j++) {
+        setLedBrightness(i, j, grid[i][j] ? 4 : 0);
+        temp[i][j] = grid[i][j];
+      }
     }
-  }
 
-  FastLED.show();
-  delay(100);
+    FastLED.show();
+    delay(100);
 
-  for (int i = 0; i < ROWS; i++) {
-    for (int j = 0; j < COLS; j++) {
-      count = countNeighbors(i, j);
-      grid[i][j] = (temp[i][j] && 2 <= count && count <= 3) || (!temp[i][j] && count == 3);
+    for (int i = 0; i < ROWS; i++) {
+      for (int j = 0; j < COLS; j++) {
+        count = countNeighbors(i, j);
+        grid[i][j] = (temp[i][j] && 2 <= count && count <= 3) || (!temp[i][j] && count == 3);
+      }
     }
   }
 }
