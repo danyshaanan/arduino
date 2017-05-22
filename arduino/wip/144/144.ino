d36f71abc4114232a2e7998a20c708b3497915b8
Dany Shaanan <danyshaanan@gmail.com>
2017-05-22 22:06:48 +0300

    Add potentiometers to strip and refactor

--- arduino/wip/144/144.ino
+++ arduino/wip/144/144.ino
@@ -1,47 +1,45 @@
-
-// Led strip animation using FastLED
-// https://github.com/FastLED/FastLED
-
 #include "FastLED.h"
 #include <stdlib.h>
 #include <math.h>
 
-#define BYTE           255         // FastLED channels are all bytes
-#define PIN            11          // data pin for led strip
+#define STRIPIN        11          // data pin for led strip
+#define POTPIN1        A0          // potentiometer for value
+#define POTPIN2        A10         // potentiometer for cycle length
+#define POTPIN3        A11         // potentiometer for cycle time
 #define NUMPIXELS      144         // number of leds on led strip
-#define fps            30          // frames per second
-#define saturation     1.00        // saturation for hsv, [0-1]
-#define value          0.25        // value for hsl, [0-1]
-#define periodInMs     8000        // ms length of period
-#define periodInPixels 72          // pixel length of period
 
+#define saturation     255         // saturation for hsv
+#define msPerFrame     33          // miliseconds per frame
+#define threshold      14          // threshold
 
-int msPerFrame = 1000 / fps;
-float timesPerMs = 1 / float(periodInMs);
-float huePerPixel = 1 / float(periodInPixels);
-
-int t;
-float hue, temp;
+float val, hps, hpp, p1, p2, p3, t1, t2, t3, hue = 0;
 CRGB leds[NUMPIXELS];
 
 /////////////////////////////////////////////////////
 
 void setup() {
-  delay(5000);
-  t = 0;
-  FastLED.addLeds<NEOPIXEL, PIN>(leds, NUMPIXELS);
+  FastLED.addLeds<NEOPIXEL, STRIPIN>(leds, NUMPIXELS);
 }
 
 void loop() {
-  for (int i=0; i<NUMPIXELS; i++) {
-    hue = timesPerMs * t - huePerPixel * i;
-    leds[i] = CHSV(BYTE * hue, BYTE * saturation, BYTE * value);
+  t1 = analogRead(POTPIN1);
+  t2 = analogRead(POTPIN2);
+  t3 = analogRead(POTPIN3);
+
+  if (t1 < p1 - threshold || t1 > p1 + threshold) p1 = t1;
+  if (t2 < p2 - threshold || t2 > p2 + threshold) p2 = t2;
+  if (t3 < p3 - threshold || t3 > p3 + threshold) p3 = t3;
+
+  val = p1 < 24 ? 0 : map(p1, 24, 1024, 64, 128);
+  hpp = p2 < 24 ? 0 : map(p2, 24, 1024, 0, 255) / 24.0;
+  hps = p3 < 24 ? 0 : map(p3, 24, 1024, 0, 255);
+
+  hue += hps * msPerFrame / 1000.0;
+
+  for (int i = 0; i < NUMPIXELS; i++) {
+    leds[i] = CHSV(hue - hpp * i, saturation, val);
   }
 
   FastLED.show();
-
-  t = int(t + msPerFrame) % periodInMs;
   delay(msPerFrame);
 }
-
-//////////////////////////////////////////////////////////
