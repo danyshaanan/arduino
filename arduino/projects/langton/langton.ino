d73b066d5a6ab444497275c33cc33a452ce81500
Dany Shaanan <danyshaanan@gmail.com>
2018-01-01 19:51:13 +0200

    Rewrite to add background and refactor

--- arduino/projects/langton/langton.ino
+++ arduino/projects/langton/langton.ino
@@ -12,19 +12,28 @@
 #define PIN             11
 #define ROWS             8
 #define COLS            32
-#define LIGHT           16 // out of 255
-#define MSPERTICK      333
+#define L               16 // out of 255
+#define MSPERTICK      250
 
 /////////////////////////////////////////////////////////
 
-struct color { float r, g, b; };
+struct color { byte r, g, b; };
+
+/////////////////////////////////////////////////////////
+
 color black, off, on;
-color linear(color from, color to, float p) {
+CRGB leds[ROWS * COLS];
+bool grid[ROWS * COLS];
+int x, y, ticks = 0, dir = 1;
+
+/////////////////////////////////////////////////////////
+
+color linear(color a, color b, float p) {
   color res;
   float q = 1 - p;
-  res.r = q * from.r + p * to.r;
-  res.g = q * from.g + p * to.g;
-  res.b = q * from.b + p * to.b;
+  res.r = q * a.r + p * b.r;
+  res.g = q * a.g + p * b.g;
+  res.b = q * a.b + p * b.b;
   return res;
 }
 
@@ -42,47 +51,48 @@ int xyToIndex(int row, int col) {
 
 /////////////////////////////////////////////////////////
 
-CRGB leds[ROWS * COLS];
-int grid[ROWS * COLS], x, y, visits, ticks = 0, dir = 2;
-
-/////////////////////////////////////////////////////////
-
-void setLed(int x, int y, color c) {
-  leds[xyToIndex(x, y)] = CRGB(LIGHT * c.g, LIGHT * c.r, LIGHT * c.b);
+bool isLedOn(int x, int y) {
+  return grid[xyToIndex(x, y)];
 }
 
 void fadeLed(int x, int y, color from, color to, int ms = MSPERTICK) {
   int frames = max(ms / 30, 1);
   for (int f = 1; f <= frames; f++) {
-    setLed(x, y, linear(from, to, 1. * f / frames));
+    color c = linear(from, to, 1. * f / frames);
+    leds[xyToIndex(x, y)] = CRGB(c.g, c.r, c.b);
     FastLED.show();
     delay(ms / frames);
   }
 }
 
-void blinkLed(int x, int y) {
-  fadeLed(x, y, black, on, 500);
-  fadeLed(x, y, on, black, 500);
+void activateLed(int x, int y, int ms = MSPERTICK) {
+  fadeLed(x, y, black, off, ms);
+}
+
+void toggleLed(int x, int y) {
+  isLedOn(x, y) ? fadeLed(x, y, on, off) : fadeLed(x, y, off, on);
+  grid[xyToIndex(x, y)] ^= true;
+}
+
+void deactivateLed(int x, int y, int ms = MSPERTICK) {
+  isLedOn(x, y) ? fadeLed(x, y, on, black, ms) : fadeLed(x, y, off, black, ms);
 }
 
 /////////////////////////////////////////////////////////
 
 void setup() {
-  switch (analogRead(0) % 4) {
-    case 0: on.r = on.g = on.b = 1; break;
-    case 1: off.r = on.g = on.b = 1; break;
-    case 2: on.r = off.g = on.b = 1; break;
-    case 3: on.r = on.g = off.b = 1; break;
+  switch (analogRead(0) % 6) {
+    case 0: off.r = on.g = on.b = L; break;
+    case 1: on.r = off.g = on.b = L; break;
+    case 2: on.r = on.g = off.b = L; break;
+    case 3: on.r = off.g = off.b = L; break;
+    case 4: off.r = on.g = off.b = L; break;
+    case 5: off.r = off.g = on.b = L; break;
   }
 
   FastLED.addLeds<WS2812, PIN>(leds, ROWS * COLS);
 
-  blinkLed(-0, -0);
-  blinkLed(-0, -1);
-  blinkLed(-1, -1);
-  blinkLed(-1, -0);
-
-//  for (x = 0; x < ROWS; x++) for (y = 0; y < COLS; y++) grid[x][y] = 0;
+  for (x = 0; x < ROWS; x++) for (y = 0; y < COLS; y++) activateLed(x, y, 5);
   x = ROWS / 2 - 1;
   y = COLS / 2 - 0;
 }
@@ -90,22 +100,21 @@ void setup() {
 /////////////////////////////////////////////////////////
 
 void loop() {
+  toggleLed(x, y);
+  
   if (dir % 4 == 0) y--;
   if (dir % 4 == 1) x--;
   if (dir % 4 == 2) y++;
   if (dir % 4 == 3) x++;
-
-  visits = grid[xyToIndex(x, y)]++;
-  dir += visits % 2 ? 1 : 3;
   
-  if      (!visits)    fadeLed(x, y, black, on);
-  else if (visits % 2) fadeLed(x, y, on, off);
-  else                 fadeLed(x, y, off, on);
+  dir += isLedOn(x, y) ? 3 : 1;
 
   ticks++;
-  if (ticks == 52) delay (3000);
-  if (ticks > 3 * 60 * 60) {
-    delay(8000);
+  if (ticks == 52) delay (1000);
+  if (ticks > 4 * 60 * 60) {
+    delay(1000);
+    for (x = 0; x < ROWS; x++) for (y = 0; y < COLS; y++) deactivateLed(x, y, 5);
+    delay(1000);
     resetFunc();
   }
 }
