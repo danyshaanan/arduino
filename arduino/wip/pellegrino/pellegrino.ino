29ff017058e1bfb35f67fbeee19376e9959d4d21
Dany Shaanan <danyshaanan@gmail.com>
2019-02-05 13:13:10 +0200

    Adjust colors

--- arduino/wip/pellegrino/pellegrino.ino
+++ arduino/wip/pellegrino/pellegrino.ino
@@ -4,7 +4,8 @@
 
 #define BYTE           255         // FastLED channels are all bytes
 #define PIN            3           // data pin for led strip
-#define NUMPIXELS      2           // number of leds on led strip
+#define NUMSENSORS     2           // number of sensors
+#define NUMPIXELS      20          // number of leds on led strip
 #define fps            30          // frames per second
 #define saturation     1           // saturation for hsv, [0-1]
 #define periodInMs     8000.       // ms length of period
@@ -13,7 +14,7 @@
 int t, input, msPerFrame = 1000 / fps;
 float hue, diff, value;
 CRGB leds[NUMPIXELS];
-int peak[NUMPIXELS] = {0};
+int peak[NUMPIXELS] = { 0 };
 int sensors[] = { 0, 1 };
 
 /////////////////////////////////////////////////////
@@ -27,14 +28,12 @@ void setup() {
 void loop() {
   hue = t / periodInMs;
 
-  for (int i=0; i<NUMPIXELS; i++) {
-    input = analogRead(sensors[i]);
-
-    if (input > sensitivity) peak[i] = t;
-
+  for (int i=0; i<NUMPIXELS; i++) leds[i] = CHSV(BYTE * hue, BYTE * saturation, BYTE * 0.2);
+  for (int i=0; i<NUMSENSORS; i++) {
+    if (analogRead(sensors[i]) > sensitivity) peak[i] = t;
     diff = (t - peak[i]) / 1000.0;
     value = peak[i] ? (diff < 1 ? diff : 1.0 / diff) : 0;
-    leds[i] = CHSV(BYTE * hue, BYTE * saturation, BYTE * (value > .1 ? value : 0));
+    if (value > .2) leds[i] = CHSV(BYTE * hue, BYTE * 0, BYTE * value);
   }
 
   FastLED.show();
